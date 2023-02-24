56f1e446fc9fb30fece8714992a50ee00f4b7288
Yonayv <fixicoyonayv@gmail.com>
2023-02-24 14:39:54 -0600

    heehee

--- Program.cs
+++ Program.cs
@@ -6,18 +6,25 @@ namespace Variables_Yo_Fix
     {
         static void Main(string[] args)
         {
+            //Declares an integer to store favorite number
             int favoriteNumber;
+            //Booleans initalized to false for jumping and running
             bool isJumping = false, isRunning = false;
+            //Declared random float variable
             float myFloat;
+            //Assigning favoriteNumber to 4
             favoriteNumber = 4;
+            //Asssigning myFloat to 54.65
             myFloat = 54.65f;
+            //Initializing constant fianlGrade to 90
             const double finalGrade = 90.0;
 
+            //Prints all variables to to console
             Console.WriteLine("Favorite #: " + favoriteNumber);
             Console.WriteLine("Jumping: " + isJumping);
             Console.WriteLine("Running: " + isRunning);
-            Console.WriteLine(myFloat);
-            Console.WriteLine(finalGrade);
+            Console.WriteLine("Random Floating #: " +myFloat);
+            Console.WriteLine("Final Grade:" + finalGrade);
         }
     }
 }
